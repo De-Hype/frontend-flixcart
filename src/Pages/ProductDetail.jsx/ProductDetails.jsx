274df667d66c9aaca2ac2d8f4b71d39b2fa5de83
De-Hype <iamsocialhype@gmail.com>
2023-11-10 02:21:22 -0800

    fixed addToCart on product details

--- src/Pages/ProductDetail.jsx/ProductDetails.jsx
+++ src/Pages/ProductDetail.jsx/ProductDetails.jsx
@@ -8,7 +8,7 @@ import {
   AiOutlineShoppingCart,
   AiOutlineStar,
 } from "react-icons/ai";
-import axios from 'axios';
+import axios from "axios";
 import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Backend_URL } from "../../server";
@@ -16,8 +16,8 @@ import { useDispatch } from "react-redux";
 import { addToCart } from "../../redux/Cartslice";
 const ProductDetails = () => {
   const [result, setResults] = useState(null);
-  const [showResult, setShowResult] = useState(false)
-  const {id}=useParams()
+  const [showResult, setShowResult] = useState(false);
+  const { id } = useParams();
   const dispatch = useDispatch();
   const handleAddToCart = (item) => {
     dispatch(addToCart(item));
@@ -25,85 +25,90 @@ const ProductDetails = () => {
   // const handleDecreaseFromCart =(item)=>{
   //   dispatch(decreaseCa(item))
   // }
- 
-  const RevealProduct = async () =>{
+
+  const RevealProduct = async () => {
     try {
-       const res = await axios.get(`${Backend_URL}/admin/product/get-product/${id}`)
+      const res = await axios.get(
+        `${Backend_URL}/admin/product/get-product/${id}`
+      );
       // const res = await axios.get(`http://localhost:7070/api/admin/product/get-product/${id}`);
-      if (res.data.status == 'ok'){
-        setResults([res.data.getProductInfo])
-        setShowResult(true)
+      if (res.data.status == "ok") {
+        setResults([res.data.getProductInfo]);
+        setShowResult(true);
       }
       // console.log(res)
     } catch (error) {
-      console.log(error)
-      setShowResult(false)
+      console.log(error);
+      setShowResult(false);
     }
-  }
+  };
 
   useEffect(() => {
-    RevealProduct()
-  }, [])
+    RevealProduct();
+  }, []);
 
-  const ShowProductDetails = ()=>{
-    return(
+  const ShowProductDetails = () => {
+    return (
       <>
-      {
-        result.map((item, index)=>(
-          <div className="product-details-top" key={index}> 
-        <img src={item.imageUrl} alt="" />
-        <div className="product-details-info">
-          <h4 className="product-details-info-name">{item.name}</h4>
-          <div className="rating">
-            <AiOutlineStar className="ratings-star" />
-            <AiOutlineStar className="ratings-star" />
-            <AiOutlineStar className="ratings-star" />
-            <AiOutlineStar className="ratings-star" />
-            <AiOutlineStar className="ratings-star" />
-          </div>
-            <h4 className="new-price">${item.new_price}</h4>
-          <p className="text">
-          {item.description}
-          </p>
-          <div className="product-details-info-socials">
-            Share :
-            <Link to="https://github.com/De-Hype">
-              <AiOutlineGithub className="product-icons" />
-            </Link>
-            <Link to="https://linkedln">
-              <AiOutlineLinkedin className="product-icons" />
-            </Link>
-            <Link to="https://david-hype.netlify.app">
-              <AiOutlineChrome  className="product-icons"/>
-            </Link>
-          </div>
-          <div className="product-details-info-cart-number">
-            <button className="cart-number">
-              <AiOutlineShoppingCart className="last-box-prop-icon"  onClick={() => handleAddToCart(item)}/>
-              Add to cart
-            </button>
-           
-             {/* <div className="product-details-info-number">
+        {result.map((item, index) => (
+          <div className="product-details-top" key={index}>
+            <img src={item.imageUrl} alt="" />
+            <div className="product-details-info">
+              <h4 className="product-details-info-name">{item.name}</h4>
+              <div className="rating">
+                <AiOutlineStar className="ratings-star" />
+                <AiOutlineStar className="ratings-star" />
+                <AiOutlineStar className="ratings-star" />
+                <AiOutlineStar className="ratings-star" />
+                <AiOutlineStar className="ratings-star" />
+              </div>
+              <h4 className="new-price">${item.new_price}</h4>
+              <p className="text">{item.description}</p>
+              <div className="product-details-info-socials">
+                Share :
+                <Link to="https://github.com/De-Hype">
+                  <AiOutlineGithub className="product-icons" />
+                </Link>
+                <Link to="https://linkedln">
+                  <AiOutlineLinkedin className="product-icons" />
+                </Link>
+                <Link to="https://david-hype.netlify.app">
+                  <AiOutlineChrome className="product-icons" />
+                </Link>
+              </div>
+              <div className="product-details-info-cart-number">
+                <button
+                  className="cart-number"
+                  onClick={() => handleAddToCart(item)}
+                >
+                  <AiOutlineShoppingCart className="last-box-prop-icon" />
+                  Add to cart
+                </button>
+
+                {/* <div className="product-details-info-number">
               <button className="cart-number-btn" onClick={() => handleAddToCart(item)}>+</button> 
               <span>{item.cartQuantity}</span>
               <button className="cart-number-btn" onClick={()=> handleDecreaseFromCart(item)}>-</button>
             </div>  */}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-        ))
-      }
+        ))}
       </>
-    )
-  }
-  
+    );
+  };
+
   return (
     <>
       <Header />
       <div className="ProductDetails">
-        {showResult? <ShowProductDetails/>: <div className="no_product_founds">
-              <h4>Invalid Product ID</h4>
-            </div>}
+        {showResult ? (
+          <ShowProductDetails />
+        ) : (
+          <div className="no_product_founds">
+            <h4>Invalid Product ID</h4>
+          </div>
+        )}
       </div>
       <Footer />
     </>
